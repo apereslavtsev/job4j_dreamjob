80bd5be6094d2c8918818a0b2bd5e9d1b60c179a
apereslavtsev <pereslavtsev.anton@gmail.com>
2023-03-30 11:04:05 +0300

    8. HTML form. Страница создания вакансии [#504844]

--- src/main/java/ru/job4j/controller/VacancyController.java
+++ src/main/java/ru/job4j/controller/VacancyController.java
@@ -19,5 +19,10 @@ public class VacancyController {
         model.addAttribute("vacancies", vacancyRepository.findAll());
         return "vacancies/list";
     }
+    
+    @GetMapping("/create")
+    public String getCreationPage() {
+        return "vacancies/create";
+    }
 
 }
